20b9b387031258459ba5111c09e8202f1031983a
aadiityaak <aadiityaak@gmail.com>
2023-10-14 08:10:47 +0700

    add package.json

---
+++ src/js/build.js
@@ -0,0 +1,58 @@
+const fs = require("fs").promises;
+const del = require("del");
+const zipdir = require("zip-dir");
+const path = require("path");
+
+async function copyDir(src, dest) {
+  await fs.mkdir(dest, { recursive: true });
+  let entries = await fs.readdir(src, { withFileTypes: true });
+  let ignore = [
+    "node_modules",
+    "dist",
+    "src",
+    ".git",
+    ".github",
+    ".gitattributes",
+    ".gitignore",
+    ".vscode",
+    "composer.json",
+    "composer.lock",
+    "package.json",
+    "package-lock.json",
+  ];
+
+  for (let entry of entries) {
+    if (ignore.indexOf(entry.name) != -1) {
+      continue;
+    }
+    let srcPath = path.join(src, entry.name);
+    let destPath = path.join(dest, entry.name);
+
+    entry.isDirectory()
+      ? await copyDir(srcPath, destPath)
+      : await fs.copyFile(srcPath, destPath);
+  }
+}
+
+(async () => {
+  try {
+    await del(["./dist"]);
+    console.log("dist is deleted!");
+
+    await copyDir("./", "./dist/velocity-custom/velocity-custom");
+
+    zipdir(
+      "./dist/velocity-custom",
+      { saveTo: "./dist/velocity-custom.zip" },
+      (err, buffer) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Zip file created");
+        }
+      }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+  }
+})();
