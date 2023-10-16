be5c0d2c39d8122f3044094aee0a5661b8c0f5cc
aadiityaak <aadiityaak@gmail.com>
2023-10-16 10:42:59 +0700

    move siteUrl, authorName,zipName as variable

--- src/build/dist-build.js
+++ src/build/dist-build.js
@@ -2,6 +2,7 @@ const { promises: fs, readFileSync, writeFileSync } = require("fs");
 const del = require("del");
 const zipdir = require("zip-dir");
 const path = require("path");
+let siteUrl, authorName, zipName;
 
 async function copyDir(src, dest, buildMode) {
   await fs.mkdir(dest, { recursive: true });
@@ -22,7 +23,7 @@ async function copyDir(src, dest, buildMode) {
   ];
 
   for (let entry of entries) {
-    if (ignore.indexOf(entry.name) != -1) {
+    if (ignore.indexOf(entry.name) !== -1) {
       continue;
     }
     let srcPath = path.join(src, entry.name);
@@ -32,37 +33,43 @@ async function copyDir(src, dest, buildMode) {
       await copyDir(srcPath, destPath, buildMode);
     } else {
       let fileContent = await fs.readFile(srcPath, "utf-8");
+      if (buildMode === "velocity") {
+        siteUrl = "https://velocitydeveloper.com";
+        authorName = "Velocity Developer";
+        zipName = "custom-plugin-velocity";
+      } else if (buildMode === "kai") {
+        siteUrl = "https://kai.web.id";
+        authorName = "Eko Mustakim";
+        zipName = "custom-plugin-kai";
+      } else {
+        siteUrl = "https://websweetstudio.com";
+        authorName = "Aditya K";
+        zipName = "custom-plugin-websweet";
+      }
 
       // Ganti semua kemunculan {REPLACE_ME_URL} dengan URL dan penulis yang sesuai dengan mode build
-      fileContent = fileContent.replace(
-        /{REPLACE_ME_URL}/g,
-        buildMode === "velocity"
-          ? "https://velocitydeveloper.com"
-          : "https://websweetstudio.com"
-      );
-      fileContent = fileContent.replace(
-        /{REPLACE_ME_AUTHOR}/g,
-        buildMode === "velocity" ? "Velocity Developer" : "Aditya K"
-      );
+      fileContent = fileContent.replace(/{REPLACE_ME_URL}/g, siteUrl);
+      fileContent = fileContent.replace(/{REPLACE_ME_AUTHOR}/g, authorName);
 
       // Tulis file yang telah dimodifikasi ke direktori tujuan
       await fs.writeFile(destPath, fileContent);
     }
   }
+
+  return zipName;
 }
 
-const buildMode = process.argv[2] || ""; // Mendapatkan mode build dari argument pertama atau default ''
+const buildMode = process.argv[2] || "";
 
 del("./dist").then(() => {
   console.log("dist is deleted!");
-  copyDir("./", "./dist/custom-plugin/custom-plugin", buildMode).then(() => {
-    let zipName =
-      buildMode === "velocity"
-        ? "custom-plugin-velocity"
-        : "custom-plugin-websweet";
-    zipdir("./dist/custom-plugin", {
-      saveTo: "./dist/" + zipName + ".zip",
-    });
-    console.log("Zip file created");
-  });
+  copyDir("./", "./dist/custom-plugin/custom-plugin", buildMode).then(
+    (resultingZipName) => {
+      zipName = resultingZipName; // Assign hasil dari copyDir ke zipName di luar fungsi
+      zipdir("./dist/custom-plugin", {
+        saveTo: `./dist/${zipName}.zip`,
+      });
+      console.log("Zip file created");
+    }
+  );
 });
